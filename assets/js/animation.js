2615c00a21b9124af7fb95fa003d55a878c337ca
Abhishek <126676277+abhi2807jhariya@users.noreply.github.com>
2026-01-30 11:12:00 +0530

    AJWebTech Website

---
+++ assets/js/animation.js
@@ -0,0 +1,8 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typing-text", {
+      strings: ["I am a Software Developer", "Web developer", "Front-End Developer", "UI Designer"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
+  });
---
+++ assets/js/include.js
@@ -0,0 +1,59 @@
+function includeHTML(file, elementId, callback) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) throw new Error("Include file not found");
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+
+      if (callback) callback(); // header load hone ke baad active set
+    })
+    .catch(err => console.error("Error loading include file:", err));
+}
+
+function setActiveMenu() {
+  const path = window.location.pathname.toLowerCase();
+
+  document.querySelectorAll(".list a").forEach(link => {
+    const href = link.getAttribute("href").toLowerCase();
+
+    if (
+      (path === "/" && (href === "/" || href === "./" || href === "index.html")) ||
+      (path.includes("about") && href.includes("about")) ||
+      (path.includes("services") && href.includes("services")) ||
+      (path.includes("contact") && href.includes("contact"))
+    ) {
+      link.classList.add("active");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  includeHTML("/assets/include/header.html", "header", setActiveMenu);
+  includeHTML("/assets/include/footer.html", "footer");
+});
+
+
+// Active btn js start
+function setActiveMenu() {
+  const currentUrl = window.location.href;
+
+  const navLinks = document.querySelectorAll('.list li a');
+
+  navLinks.forEach(link => {
+    const linkUrl = link.href;
+
+    if (currentUrl === linkUrl || currentUrl === linkUrl + '/') {
+      link.classList.add('menu_actve');
+    } else if (link.getAttribute('href') !== '/' && currentUrl.includes(link.getAttribute('href'))) {
+      link.classList.add('menu_actve');
+    } else {
+      link.classList.remove('menu_actve');
+    }
+  });
+}
+window.addEventListener('load', setActiveMenu);
+
+
+// Active btn js end
